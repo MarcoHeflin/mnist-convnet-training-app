2ca0a0c636c8f6e00d6a3a9f889f5b06a0034ef4
MarcoHeflin <42918384+MarcoHeflin@users.noreply.github.com>
2022-04-28 21:56:41 -0400

    added 2d softmax output visualization using the grand tour method

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
 import pickle
-import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
 from keras import layers
 import os
-import time
+import sympy as sp
+from matplotlib.widgets import Slider
+
+def scos(x): return sp.N(sp.cos(x))
+def ssin(x): return sp.N(sp.sin(x))
 
 # path where we will store any models we plan to save and their history
 # this is where all files for this application will be stored.
@@ -58,9 +61,18 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 #print(y_train[:100])
 
-# Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+# custom keras callback to get the model's prediction vectors at the end of each epoch for all the training samples
+# this will be used in our Grand Tour visualization method
+class TestPredictionsCallback(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.test_predictions = []
+    def on_epoch_end(self, epoch, logs=None):
+        # use the model at this point to get the softmax output
+        # for all of the testing samples
+        y_pred_test = self.model.predict(x_test)
+        self.test_predictions.append(y_pred_test)
+    def get_data(self):
+        return self.test_predictions
 
 # function that builds and trains a model then returns the trained model and the history
 def trainNewModel(model_name="model", learning_rate=0.01, batch_size=128, momentum=0.0, epochs=10):
@@ -84,16 +96,21 @@ def trainNewModel(model_name="model", learning_rate=0.01, batch_size=128, moment
     #model.summary()
     model._name = model_name
 
+    # use the custom callback fn. to get the test predictions at the end of each epoch
+    testPredictionsCallback = TestPredictionsCallback()
+
     # train the model
     model.compile(loss="categorical_crossentropy",
                   optimizer=keras.optimizers.SGD(learning_rate=learning_rate, momentum=momentum), metrics=["accuracy"])
 
     newModelHistory = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
-                        validation_data=(x_test, y_test)).history
+                        validation_data=(x_test, y_test), callbacks=[testPredictionsCallback]).history
+
+    test_predictions = testPredictionsCallback.get_data()
 
     # list all data in history
     # print(history.keys())
-    return model, newModelHistory
+    return model, newModelHistory, test_predictions
 
 # function to get the accuracy and loss plots from the model history
 def getAccuracyFig(history, model, epochs):
@@ -119,7 +136,7 @@ def getLossFig(history, model, epochs):
     ax = lossFigure.add_subplot(111)
     ax.plot(history['loss'], color='r', linestyle='dashed')
     ax.plot(history['val_loss'], color='darkblue')
-    title = model.name + ' Loss (categorical cross-entropy) : ' + f'Best Loss = {loss:.3}'
+    title = model.name + ' Loss (categorical cross-entropy) : ' + f'Final Loss = {loss:.3}'
     ax.set_title(title, loc='center')
     ax.set_ylabel('loss')
     ax.set_xlabel('epoch')
@@ -154,6 +171,119 @@ def loadModel(model_name):
     currentModelDict = pickle.load(open(storage_path+model_name+"_info.p", "rb"))
     currentHistory = pickle.load(open(storage_path+model_name+"_history.p", "rb"))
     currentModel = load_model(storage_path+model_name+'.h5')
+
+# vector to be used in the visualization of the softmax layers output for each epoch using grand tour method.
+# essentially 10 equinormal vectors from the origin to represent the 10 different classes/dimensions.
+# representing a 10 dimensional space with 10 2-dimensional vectors
+dimensions_vectors = np.array([[1,0],[scos(sp.pi/5),ssin(sp.pi/5)],[scos(2*sp.pi/5),ssin(2*sp.pi/5)],[scos(3*sp.pi/5),ssin(3*sp.pi/5)],
+              [scos(4*sp.pi/5),ssin(4*sp.pi/5)],[scos(5*sp.pi/5),ssin(5*sp.pi/5)],[scos(6*sp.pi/5),ssin(6*sp.pi/5)],
+              [scos(7*sp.pi/5),ssin(7*sp.pi/5)],[scos(8*sp.pi/5),ssin(8*sp.pi/5)],[scos(9*sp.pi/5),ssin(9*sp.pi/5)]])
+dimensions_vectors = np.array(dimensions_vectors, dtype=float)
+#print(dimensions_vectors.shape)
+
+# colors to be used in the grand tour softmax visualization
+colors = ['b','r','grey', 'g', 'c', 'm', 'y', 'indigo', 'greenyellow', 'darkorange']
+
+# function to get a list where list is the size of the number of epochs for the model predictions
+# and each index of the list has a dictionary with keys 0-9 as the true label for each sample
+# where value is a list of predictions for each test sample with that true label
+def getPredictionsDictionariesList(predictions):
+    #print(len(predictions))
+    #print(len(predictions[0]))
+    prediction_dictionaries_list = []
+    for i in range(len(predictions)):       # iterate thru number of epochs
+        # get the points for all of the predictions and group them by true label (correct class 0-9)
+        # for this epoch
+        predictions_dict = {l: {"X": [], "Y": []} for l in range(10)}
+
+        for j, prediction in enumerate(predictions[i]):     # iterate through the testing samples for this epoch
+            sumX = 0
+            sumY = 0
+            # get the x,y for this prediction based on our 10-dimensional space
+            for k in range(10):
+                sumX += prediction[k] * dimensions_vectors[k][0]
+                sumY += prediction[k] * dimensions_vectors[k][1]
+
+            true_label = np.argmax(y_test[j])
+
+            # print(true_label)
+            predictions_dict[true_label]["X"].append(sumX)
+            predictions_dict[true_label]["Y"].append(sumY)
+
+        prediction_dictionaries_list.append(predictions_dict)
+
+    #print(prediction_dictionaries_list)
+    return prediction_dictionaries_list
+
+# function that displays/returns a plot to visualize the output of the softmax layer on the testing set at each epoch. Grand Tour method.
+def visualizeSoftmaxLayerWithGrandTour(predictions):
+    # predictions_dict initially for first epoch
+    predictions_dict_list = getPredictionsDictionariesList(predictions)
+
+    predictions_dict = predictions_dict_list[0]
+
+    fig = plt.figure()
+    ax = fig.subplots()
+
+    plt.subplots_adjust(bottom = 0.5)  # create some space at bottom to insert the slider button
+
+    for i in range(10):
+        ax.arrow(0.0, 0.0, dimensions_vectors[i][0], dimensions_vectors[i][1], head_width=0.0, head_length=0.0, color=colors[i], length_includes_head=True)
+        # ax.plot(predictions_dict[i]["X"], predictions_dict[i]["Y"], "o", markersize=2, color=colors[0], label=str(0))
+
+    p0, = ax.plot(predictions_dict[0]["X"], predictions_dict[0]["Y"], "o", markersize=2, color=colors[0], label=str(0))
+    p1, = ax.plot(predictions_dict[1]["X"], predictions_dict[1]["Y"], "o", markersize=2, color=colors[1], label=str(1))
+    p2, = ax.plot(predictions_dict[2]["X"], predictions_dict[2]["Y"], "o", markersize=2, color=colors[2], label=str(2))
+    p3, = ax.plot(predictions_dict[3]["X"], predictions_dict[3]["Y"], "o", markersize=2, color=colors[3], label=str(3))
+    p4, = ax.plot(predictions_dict[4]["X"], predictions_dict[4]["Y"], "o", markersize=2, color=colors[4], label=str(4))
+    p5, = ax.plot(predictions_dict[5]["X"], predictions_dict[5]["Y"], "o", markersize=2, color=colors[5], label=str(5))
+    p6, = ax.plot(predictions_dict[6]["X"], predictions_dict[6]["Y"], "o", markersize=2, color=colors[6], label=str(6))
+    p7, = ax.plot(predictions_dict[7]["X"], predictions_dict[7]["Y"], "o", markersize=2, color=colors[7], label=str(7))
+    p8, = ax.plot(predictions_dict[8]["X"], predictions_dict[8]["Y"], "o", markersize=2, color=colors[8], label=str(8))
+    p9, = ax.plot(predictions_dict[9]["X"], predictions_dict[9]["Y"], "o", markersize=2, color=colors[9], label=str(9))
+
+    # Defining the slider button
+    ax_slide = plt.axes([0.15, 0.2, .6, .05])     # xpos, ypos, width, height
+    # Properties of the slider
+    epochs_slider = Slider(ax_slide, 'Epoch', valmin=1, valmax=len(predictions), valinit=1, valstep=1)
+
+    # function to be called whenever the value of the slider changes. updates the plot for new epoch #
+    def update(val):
+        new_epoch = epochs_slider.val
+        predictions_dict = predictions_dict_list[new_epoch-1]
+        p0.set_xdata(predictions_dict[0]["X"])
+        p0.set_ydata(predictions_dict[0]["Y"])
+        p1.set_xdata(predictions_dict[1]["X"])
+        p1.set_ydata(predictions_dict[1]["Y"])
+        p2.set_xdata(predictions_dict[2]["X"])
+        p2.set_ydata(predictions_dict[2]["Y"])
+        p3.set_xdata(predictions_dict[3]["X"])
+        p3.set_ydata(predictions_dict[3]["Y"])
+        p4.set_xdata(predictions_dict[4]["X"])
+        p4.set_ydata(predictions_dict[4]["Y"])
+        p5.set_xdata(predictions_dict[5]["X"])
+        p5.set_ydata(predictions_dict[5]["Y"])
+        p6.set_xdata(predictions_dict[6]["X"])
+        p6.set_ydata(predictions_dict[6]["Y"])
+        p7.set_xdata(predictions_dict[7]["X"])
+        p7.set_ydata(predictions_dict[7]["Y"])
+        p8.set_xdata(predictions_dict[8]["X"])
+        p8.set_ydata(predictions_dict[8]["Y"])
+        p9.set_xdata(predictions_dict[9]["X"])
+        p9.set_ydata(predictions_dict[9]["Y"])
+        fig.canvas.draw()
+
+    epochs_slider.on_changed(update)
+
+    # TODO: add a play/pause button, couldnt get it to work before
+
+    # plt.plot(0,0,'ok') #<-- plot a black point at the origin
+    # plt.axis('equal')  #<-- set the axes to the same scale
+    ax.set_xlim([-1.25, 1.5])  # <-- set the x axis limits
+    ax.set_ylim([-1.25, 1.25])  # <-- set the y axis limits
+    ax.grid(visible=False, which='major')  # <-- plot grid lines
+    ax.legend(loc="best", title="Digits")
+    return fig
 
 # build the tkinter window for our GUI
 root = Tk()
@@ -225,7 +355,7 @@ def openResultsPage():
     if saved_model_name == "":
         openHomePage()
         return
-    print("saved model name selected: {0}".format(saved_model_name))
+    #print("saved model name selected: {0}".format(saved_model_name))
     loadModel(saved_model_name)
 
     # first put a label for the title of this screen
@@ -256,6 +386,47 @@ def openResultsPage():
     Button(results_frame, text="Back to Home", command=openHomePage, font=MEDIUM_FONT).pack(pady=10)
     results_frame.pack(fill="both", expand=1)
 
+def openGrandTourPage():
+    numTestSamples = int(number_of_points_entry.get().strip())
+    hide_all_frames()
+    # get the saved models name we want to load from the spinner then load it from the saved files
+    #saved_model_name = saved_models_spinner.get()
+    saved_model_name = saved_models_spinner.get()
+    if saved_model_name == "":
+        openHomePage()
+        return
+    #print("saved model name selected: {0}".format(saved_model_name))
+    loadModel(saved_model_name)
+
+    # first put a label for the title of this screen
+    Label(results_frame, text="2D Grand Tour Softmax Visualization for "+str(currentModel.name), font=LARGE_FONT).pack(pady=10)
+
+    Label(results_frame, text="Training Hyperparameters", font=MEDIUM_FONT).pack(pady=10)
+    epochs = currentModelDict["epochs"]
+    Label(results_frame, text="Number of epochs: "+str(epochs), font=SMALL_FONT).pack()
+    Label(results_frame, text="Learning rate: "+str(currentModelDict['learning_rate']), font=SMALL_FONT).pack()
+    Label(results_frame, text="Batch size: "+str(currentModelDict['batchSize']), font=SMALL_FONT).pack()
+    Label(results_frame, text="Momentum: "+str(currentModelDict['momentum']), font=SMALL_FONT).pack(pady=10)
+    accuracy = currentModelDict["accuracy"]
+    loss = currentModelDict["loss"]
+    Label(results_frame, text=f'Top Accuracy = {accuracy:.3}', font=MEDIUM_FONT).pack(pady=10)
+    Label(results_frame, text=f'Final Loss = {loss:.3}', font=MEDIUM_FONT).pack(pady=10)
+
+    test_predictions = np.array(currentModelDict["test_predictions"])
+    #print(len(test_predictions))       # len of first dimension is number of epochs
+    #print(len(test_predictions[0]))    # len of 2nd is number of test samples. 10000
+    #print(len(test_predictions[0][0])) # len is 10 representing the softmax output for each test sample
+
+    softmax_visualization = visualizeSoftmaxLayerWithGrandTour(test_predictions[:, :numTestSamples])   # visualize with just 1000 test samples for now to improve efficiency
+    # creating Tkinter canvas to display the Matplotlib figures
+    canvas1 = FigureCanvasTkAgg(softmax_visualization, master = results_frame)
+    # placing the canvas on the Tkinter window
+    canvas1.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1, pady=10, padx=100)
+
+    # get the figure for the grand tour softmax visualizaiton
+    Button(results_frame, text="Back to Home", command=openHomePage, font=MEDIUM_FONT).pack(pady=10)
+    results_frame.pack(fill="both", expand=1)
+
 # function to open the info page
 def openInfoFrame():
     hide_all_frames()
@@ -269,6 +440,7 @@ def hide_all_frames():
     learningRateEntry.delete(0, END)
     momentumEntry.delete(0, END)
     epochsEntry.delete(0, END)
+    number_of_points_entry.delete(0,END)
     # forget the pack for all the frames. essentially clears every frame from the root window.
     homePage.pack_forget()
     training_frame.pack_forget()
@@ -285,10 +457,10 @@ def hide_all_frames():
 # fn. to begin training a new model from the training frame upon clicking begin training button
 def trainModel():
     modelName = modelNameEntry.get()
-    numEpochs = int(epochsEntry.get())
-    learningRate = float(learningRateEntry.get())
-    batchSize = int(batchSizeEntry.get())
-    momentum = float(momentumEntry.get())
+    numEpochs = int(epochsEntry.get().strip())
+    learningRate = float(learningRateEntry.get().strip())
+    batchSize = int(batchSizeEntry.get().strip())
+    momentum = float(momentumEntry.get().strip())
     # if this model already exists print an error to the user saying model name already exists
     if modelName in saved_models:
         Label(training_frame, "Model name already exists in your saved models!!!", bg="red", font=SMALL_FONT).pack(pady=10)
@@ -298,7 +470,7 @@ def trainModel():
     openLoadingFrame()
 
     # now train the model with the user input hyperparameters
-    model, history = trainNewModel(model_name=modelName, learning_rate=learningRate, batch_size=batchSize, momentum=momentum, epochs=numEpochs)
+    model, history, test_predictions = trainNewModel(model_name=modelName, learning_rate=learningRate, batch_size=batchSize, momentum=momentum, epochs=numEpochs)
     # Evaluate the trained model
     loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
     # set this trained model and its history as the global variables for the current model/history
@@ -310,7 +482,8 @@ def trainModel():
         "momentum": momentum,
         "epochs": numEpochs,
         "loss": loss,
-        "accuracy": accuracy
+        "accuracy": accuracy,
+        "test_predictions": test_predictions
     }
     global currentModel, currentHistory, currentModelDict
     currentModel, currentHistory = model, history
@@ -326,9 +499,16 @@ def deleteModelAndGoToHome():
         os.remove(storage_path+modelName+"_history.p")
     if os.path.exists(storage_path+modelName+"_info.p"):
         os.remove(storage_path+modelName+"_info.p")
+    global saved_models
     saved_models.remove(modelName)
     saved_models_spinner.config(values=saved_models)
+    if len(saved_models) == 0:
+        saved_models_spinner.config(values=[])
     openHomePage()
+
+def exitProgramGracefully():
+    root.destroy()
+    exit()
 
 # first I make a frame for the homepage
 homePage = Frame(root, width=screen_width, height=screen_height, bg="gray")
@@ -337,7 +517,7 @@ titleLabel.pack(pady=20)
 Button(homePage, text="App Info", command=openInfoFrame, font=MEDIUM_FONT).pack(pady=20)
 Button(homePage, text="Train New Model", command=openTrainingFrame, font=MEDIUM_FONT).pack(pady=10)
 Button(homePage, text="Saved Models", command=openSavedModelsFrame, font=MEDIUM_FONT).pack(pady=20)
-Button(homePage, text="Quit", command=root.destroy).pack(pady=10)  # button to quit amicably from the program
+Button(homePage, text="Quit", command=exitProgramGracefully).pack(pady=10)  # button to quit amicably from the program
 
 # loading frame for when a model is being trained to provide a loading screen
 loading_frame = Frame(root, width=screen_width, height=screen_height, bg="gray")
@@ -382,12 +562,17 @@ Button(training_frame, text="Back to Home", command=openHomePage, font=MEDIUM_FO
 saved_models_frame = Frame(root, width=screen_width, height=screen_height, bg="gray")
 Label(saved_models_frame, text="Saved Models", font=LARGE_FONT).pack(pady=10)
 saved_models_spinner = Spinbox(saved_models_frame, values=saved_models, font=LARGE_FONT, state = 'readonly')
-saved_models_spinner.pack(pady=10)
-Button(saved_models_frame, text="Load Selected Model", command=openResultsPage, bg="green", font=MEDIUM_FONT).pack(
+saved_models_spinner.pack(pady=30)
+Label(saved_models_frame, text="Number of Points to Display (only applicable for Grand Tour Visualization; there are 10,000 test samples. Enter number b/w 100-10,000):", font=SMALL_FONT).pack(pady=5)
+number_of_points_entry = Entry(saved_models_frame, width=50)
+number_of_points_entry.pack(pady=5)
+Button(saved_models_frame, text="Visualize with the Grand Tour", command=openGrandTourPage, bg="light blue", font=MEDIUM_FONT).pack(
     pady=20)
-Button(saved_models_frame, text="Delete Selected Model", command=deleteModelAndGoToHome, bg="red", font=MEDIUM_FONT).pack(
+Button(saved_models_frame, text="Load Selected Model", command=openResultsPage, bg="green", font=MEDIUM_FONT).pack(
     pady=10)
-Button(saved_models_frame, text="Back to Home", command=openHomePage, font=MEDIUM_FONT).pack(pady=20)
+Button(saved_models_frame, text="Delete Selected Model", command=deleteModelAndGoToHome, bg="indian red", font=MEDIUM_FONT).pack(
+    pady=20)
+Button(saved_models_frame, text="Back to Home", command=openHomePage, font=MEDIUM_FONT).pack(pady=10)
 
 # create a frame for the results page
 results_frame = Frame(root, width=screen_width, height=screen_height, bg="gray")
